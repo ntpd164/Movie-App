bd2bc4b8a5d6bc80fe1c2b2acf0dcff5d185b121
Your Name <you@example.com>
2024-07-08 18:54:44 +0700

    Sort movie in watchlist

--- src/pages/WatchList.jsx
+++ src/pages/WatchList.jsx
@@ -1,18 +1,92 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import {
+  faArrowLeft,
+  faStar,
+  faChevronCircleDown,
+  faUpLong,
+  faDownLong,
+} from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import Footer from '../ui/Footer';
 
 export default function WatchList({ username }) {
   const navigate = useNavigate();
-
-  const watched = JSON.parse(localStorage.getItem('watched')) || [];
+  const [showOrderMenu, setShowOrderMenu] = useState(false);
+  const [order, setOrder] = useState('List order');
+  const [sortDirection, setSortDirection] = useState('asc');
+  const [watched, setWatched] = useState(
+    JSON.parse(localStorage.getItem('watched')) || []
+  );
   console.log('Watched: ', watched);
 
   document.title = 'Your Watchlist';
 
   function handleBack() {
     navigate('/');
+  }
+
+  function handleShowOrderMenu() {
+    setShowOrderMenu(!showOrderMenu);
+  }
+
+  function handleOrderChange(order) {
+    setOrder(order);
+    setShowOrderMenu(false);
+    setSortDirection('asc');
+    sortWatched(order, 'asc');
+  }
+
+  function handleSortDirection() {
+    setSortDirection((prevDirection) => {
+      const newDirection = prevDirection === 'asc' ? 'desc' : 'asc';
+      sortWatched(order, newDirection);
+      return newDirection;
+    });
+  }
+
+  function sortWatched(order, direction) {
+    let sortedWatched = [...watched];
+    switch (order) {
+      case 'Alphabetical':
+        sortedWatched.sort((a, b) =>
+          direction === 'asc'
+            ? a.title.localeCompare(b.title)
+            : b.title.localeCompare(a.title)
+        );
+        break;
+      case 'IMDb rating':
+        sortedWatched.sort((a, b) =>
+          direction === 'asc'
+            ? a.imdbRating - b.imdbRating
+            : b.imdbRating - a.imdbRating
+        );
+        break;
+      case 'Release date':
+        sortedWatched.sort((a, b) =>
+          direction === 'asc' ? a.year - b.year : b.year - a.year
+        );
+        break;
+      case 'Runtime':
+        sortedWatched.sort((a, b) =>
+          direction === 'asc' ? a.runtime - b.runtime : b.runtime - a.runtime
+        );
+        break;
+      case 'Your rating':
+        sortedWatched.sort((a, b) =>
+          direction === 'asc'
+            ? a.userRating - b.userRating
+            : b.userRating - a.userRating
+        );
+        break;
+      default:
+        sortedWatched = JSON.parse(localStorage.getItem('watched')) || [];
+        if (direction === 'desc') {
+          sortedWatched.reverse();
+        }
+        break;
+    }
+    setWatched(sortedWatched);
   }
 
   return (
@@ -42,9 +116,96 @@ export default function WatchList({ username }) {
         </p>
       </div>
       <div className=" bg-white pb-20 pl-28 pt-28">
-        <div className="mb-10 font-poppins-semibold text-3xl font-normal text-black">
-          <span>{watched.length}</span>
-          {watched.length > 1 ? ' movies' : ' movie'}
+        <div className="mb-10 flex font-poppins-semibold text-3xl font-normal text-black">
+          <div className="mt-3">
+            <span>{watched.length}</span>
+            {watched.length > 1 ? ' movies' : ' movie'}
+          </div>
+          <div className="ml-[60rem] flex">
+            <span className="mt-3">Sort by</span>
+            <div
+              onClick={handleShowOrderMenu}
+              className="relative ml-8 cursor-pointer px-4 py-3 text-[#0472d2] hover:bg-[#ededed]"
+            >
+              <button className="">{order}</button>
+              <FontAwesomeIcon icon={faChevronCircleDown} className="ml-3" />
+              {showOrderMenu && (
+                <div className="absolute -left-6 mt-2 w-[120%] rounded-md border border-zinc-400 bg-white font-poppins-medium font-light text-black">
+                  <div
+                    onClick={() => handleOrderChange('List order')}
+                    className={`px-4 py-2 ${
+                      order === 'List order' ? 'bg-[#ededed]' : ''
+                    } hover:bg-[#ededed]`}
+                  >
+                    List order
+                  </div>
+                  <div
+                    onClick={() => handleOrderChange('Alphabetical')}
+                    className={`px-4 py-2 ${
+                      order === 'Alphabetical' ? 'bg-[#ededed]' : ''
+                    } hover:bg-[#ededed]`}
+                  >
+                    Alphabetical
+                  </div>
+                  <div
+                    onClick={() => handleOrderChange('IMDb rating')}
+                    className={`px-4 py-2 ${
+                      order === 'IMDb rating' ? 'bg-[#ededed]' : ''
+                    } hover:bg-[#ededed]`}
+                  >
+                    IMDb rating
+                  </div>
+                  <div
+                    onClick={() => handleOrderChange('Release date')}
+                    className={`px-4 py-2 ${
+                      order === 'Release date' ? 'bg-[#ededed]' : ''
+                    } hover:bg-[#ededed]`}
+                  >
+                    Release date
+                  </div>
+                  <div
+                    onClick={() => handleOrderChange('Runtime')}
+                    className={`px-4 py-2 ${
+                      order === 'Runtime' ? 'bg-[#ededed]' : ''
+                    } hover:bg-[#ededed]`}
+                  >
+                    Runtime
+                  </div>
+                  <div
+                    onClick={() => handleOrderChange('Your rating')}
+                    className={`px-4 py-2 ${
+                      order === 'Your rating' ? 'bg-[#ededed]' : ''
+                    } hover:bg-[#ededed]`}
+                  >
+                    Your rating
+                  </div>
+                </div>
+              )}
+            </div>
+            <div className="relative">
+              <div
+                onClick={handleSortDirection}
+                className="absolute -bottom-3 left-4 cursor-pointer rounded-full pb-5 pr-9 pt-10 hover:bg-[#ededed]"
+              >
+                <FontAwesomeIcon
+                  icon={faUpLong}
+                  className={`absolute top-5 ml-8 ${
+                    sortDirection === 'asc'
+                      ? 'text-[#0472d2]'
+                      : 'text-[#7a7a7a]'
+                  }`}
+                />
+                <FontAwesomeIcon
+                  icon={faDownLong}
+                  className={`ml-12 ${
+                    sortDirection === 'desc'
+                      ? 'text-[#0472d2]'
+                      : 'text-[#7a7a7a]'
+                  }`}
+                />
+              </div>
+            </div>
+          </div>
         </div>
         <div className="w-2/3 rounded-md border border-zinc-400 pb-10 pt-14">
           {watched.map((movie, index) => (
@@ -63,10 +224,20 @@ export default function WatchList({ username }) {
                     <span className="mr-6">{movie.year}</span>
                     <span>{movie.runtime} minutes</span>
                   </div>
-                  <div className="-ml-1 mt-4 flex items-center font-poppins-medium text-2xl font-light text-[#7a7a7a]">
-                    <span>⭐️</span>
-                    <span className="ml-1 mr-2">{movie.imdbRating}</span>
-                    <span>({movie.imdbVotes})</span>
+                  <div className="relative -ml-1 mt-4 flex items-center font-poppins-medium text-2xl font-light text-[#7a7a7a]">
+                    <FontAwesomeIcon
+                      icon={faStar}
+                      className="absolute left-1 top-[1px] text-primary"
+                    />
+                    <span className="ml-10 mr-2">{movie.imdbRating}</span>
+                    <span className="">({movie.imdbVotes})</span>
+                    <div className="relative ml-14">
+                      <FontAwesomeIcon
+                        icon={faStar}
+                        className="absolute left-0 top-[1px] text-[#116bd9]"
+                      />
+                      <span className="ml-9">{movie.userRating}</span>
+                    </div>
                   </div>
                 </div>
               </div>
