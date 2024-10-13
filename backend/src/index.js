cbc79a1a61f5e1be235feecb9fc2e7e629e6b987
ntpd164 <duongngo1616@gmail.com>
2024-10-14 01:58:22 +0700

    fix bug

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,6 +22,8 @@ app.use(
   })
 );
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -50,6 +52,10 @@ route(app);
 
 app.get('/test', (req, res) => {
   res.json({ message: 'Hello world' });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(port, () => {
